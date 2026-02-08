1e7057d8f5aad4b953aaf2c5fcd050fc3bf52c40
Otto <danielottojackpetersen2@gmail.com>
2026-02-08 20:38:28 +0000

    docs: storybook tidy

--- src/components/task/task.component.ts
+++ src/components/task/task.component.ts
@@ -10,8 +10,23 @@ import { NgOptimizedImage } from "@angular/common";
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
+  /**
+   * Text to be displayed in the head slot
+   */
   head = input.required<string>();
+
+  /**
+   * Optional text to override the "Visit app" call to action
+   */
   callToAction = input<string>("Visit app");
+
+  /**
+   * Media source e.g. `"/my-picture.png"`
+   */
   media = input.required<string>();
+
+  /**
+   * Screen reader text for `media`
+   */
   mediaAlt = input.required<string>();
 }
--- src/components/task/task.stories.ts
+++ src/components/task/task.stories.ts
@@ -2,13 +2,25 @@ import {Meta, moduleMetadata, StoryObj} from '@storybook/angular';
 import {TaskComponent} from "./task.component";
 import { NgOptimizedImage } from "@angular/common";
 
+/**
+ * The Task is a component that is applied to an interactive parent e.g. `<button task` or `<a task`.
+ * See the description of each control for further details.
+ *
+ * > Compatible with dark mode theme
+ */
 const meta: Meta<TaskComponent> = {
   title: 'Components/Task',
   component: TaskComponent,
   tags: ['autodocs'],
   decorators: [moduleMetadata({
     imports: [NgOptimizedImage]
-  })]
+  })],
+  args: {
+    head: "Head text",
+    media: "/Upload Data.png",
+    mediaAlt: "upload data icon with pink background",
+    callToAction: "Visit app",
+  }
 };
 
 export default meta;
@@ -18,9 +30,28 @@ export const Default: Story = {
   render: (args) => ({
     props: args,
     template: `
-      <button task head="Upload data" callToAction="Visit app">
-          <img slot="media" ngSrc="/Upload Data.png" height="144" width="128" alt="upload data icon with pink background"/>
-          Submit member and cashflow data to Hymans Robertson via our single secure data submission portal with real-time data validation.
+      <button task [head]="head" [callToAction]="callToAction" [media]="media" [mediaAlt]="mediaAlt">
+        Body text
       </button>`
+  })
+}
+
+export const WithLargeBodyText: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <button task [head]="head" [callToAction]="callToAction" [media]="media" [mediaAlt]="mediaAlt">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+      </button>`
+  })
+}
+
+export const AsLink: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <a href="#" task [head]="head" [callToAction]="callToAction" [media]="media" [mediaAlt]="mediaAlt">
+        This task is a link
+      </a>`
   })
 }
