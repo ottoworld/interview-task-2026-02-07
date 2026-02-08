ef894a7d2493d4859467a7f14823b2f024fe9204
Otto <danielottojackpetersen2@gmail.com>
2026-02-08 20:15:51 +0000

    feat: page final content

--- src/app/app.ts
+++ src/app/app.ts
@@ -32,30 +32,30 @@ export class App {
       onClick: this.onClick
     },
     {
-      id: "upload",
-      body: "Submit member and cashflow data to Hymans Robertson via our single secure data submission portal with real-time data validation.",
-      head: "Upload data",
+      id: "see",
+      body: "Access all your advice and documents from Hymans Roberson via our secure online storage facility.",
+      head: "See my documents",
       callToAction: "Visit app",
-      media: "/Upload Data.png",
-      mediaAlt: "Upload data icon with pink background",
+      media: "/See my documents.png",
+      mediaAlt: "Documents icon with yellow background",
       onClick: this.onClick
     },
     {
-      id: "upload",
-      body: "Submit member and cashflow data to Hymans Robertson via our single secure data submission portal with real-time data validation.",
-      head: "Upload data",
+      id: "whole",
+      body: "Monitor and view latest whole fund funding position.",
+      head: "Whole fund funding update",
       callToAction: "Visit app",
-      media: "/Upload Data.png",
-      mediaAlt: "Upload data icon with pink background",
+      media: "/Whole fund update.png",
+      mediaAlt: "Graph icon with blue background",
       onClick: this.onClick
     },
     {
-      id: "upload",
-      body: "Submit member and cashflow data to Hymans Robertson via our single secure data submission portal with real-time data validation.",
-      head: "Upload data",
+      id: "employer",
+      body: "Calculate an updated funding level or indicative cessation valuation for any employer. View and download reports to share with your employers",
+      head: "Employer funding update",
       callToAction: "Visit app",
-      media: "/Upload Data.png",
-      mediaAlt: "Upload data icon with pink background",
+      media: "/Employer funding update.png",
+      mediaAlt: "Employer connections icon with green background",
       onClick: this.onClick
     },
   ]
